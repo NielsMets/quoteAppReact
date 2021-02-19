d00a251e2f0fc51a41fe48d10279675b669117ed
Niels Metselaar <metsniels@gmail.com>
2021-02-19 12:33:26 +0100

    added min value to color ranges

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import "./styles/style.css";
 
 function App() {
   //state
-
   const [num, setNum] = useState(0);
   const [quote, setQuote] = useState(quotes[0].quote);
   const [author, setAuthor] = useState(quotes[0].author);
   const [bgColor, setBgColor] = useState("white");
 
   useEffect(() => {
-    setBgColor(randomHSL(360, 40, 70));
+    setBgColor(randomHSL(360, 0, 40, 0, 70, 40));
   }, []);
 
   //get random quote and color
@@ -21,15 +20,19 @@ function App() {
 
     setQuote(quotes[num].quote);
     setAuthor(quotes[num].author);
-    setBgColor(randomHSL(360, 40, 70));
+    setBgColor(randomHSL(360, 0, 40, 0, 60, 40));
   };
 
   //generate random HSL color
-  const randomHSL = (h, s, l) => {
-    let randomNum = (max) => {
-      return Math.floor(Math.random() * max);
+  const randomHSL = (h, hMin, s, sMin, l, lMin) => {
+    let randomNum = (max, min) => {
+      let num = Math.floor(Math.random() * max);
+      if (num < min) {
+        num = min;
+      }
+      return num;
     };
-    return `hsl(${randomNum(h)},${randomNum(s)}%,${randomNum(l)}%)`;
+    return `hsl(${randomNum(h)},${randomNum(s)}%,${randomNum(l, lMin)}%)`;
   };
 
   //generate random list index number
