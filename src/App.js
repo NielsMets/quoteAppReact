fe57347f50b2cecc0160516df326e576fe9be5e2
Niels Metselaar <metsniels@gmail.com>
2021-07-01 14:57:59 +0200

    refactor complete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React, { useEffect, useState } from "react";
-import QuoteBox from "./components/QuoteBox";
 import "./styles/style.css";
-import quotes from "./quotes.json";
-
-import { randomListIndex } from "./functions/randomListIndex";
+import QuoteBox from "./components/QuoteBox";
 import { randomHSL } from "./functions/randomHSL";
 
 function App() {
   //state
-  const [num, setNum] = useState(0);
-  const [quote, setQuote] = useState(quotes.quotes[0].quote);
-  const [author, setAuthor] = useState(quotes.quotes[0].author);
   const [bgColor, setBgColor] = useState("white");
 
   useEffect(() => {
-    randomQuote();
-  }, []);
-
-  //get random quote and color
-  const randomQuote = () => {
-    setNum(randomListIndex(quotes.quotes, num, setNum));
-
-    setQuote(quotes.quotes[num].quote);
-    setAuthor(quotes.quotes[num].author);
     setBgColor(randomHSL(360, 0, 50, 0, 60, 40));
-  };
+  }, []);
 
   return (
     <section style={{ backgroundColor: bgColor }}>
-      <QuoteBox
-        randomQuote={randomQuote}
-        quote={quote}
-        author={author}
-        bgColor={bgColor}
-      />
+      <QuoteBox bgColor={bgColor} setBgColor={setBgColor} />
     </section>
   );
 }
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import quotes from "../data/quotes.json";
 import Button from "./Button";
 import Author from "./Author";
 import Quote from "./Quote";
+import { randomListIndex } from "../functions/randomListIndex";
+import { randomHSL } from "../functions/randomHSL";
 
 export default function (props) {
-  const { quote, author, randomQuote, bgColor } = props;
+  const { bgColor, setBgColor } = props;
+  const [num, setNum] = useState(0);
+  const [quote, setQuote] = useState(quotes.quotes[0].quote);
+  const [author, setAuthor] = useState(quotes.quotes[0].author);
+
+  useEffect(() => {
+    randomQuote();
+  }, []);
+
+  const randomQuote = () => {
+    setBgColor(randomHSL(360, 0, 50, 0, 60, 40));
+    setNum(randomListIndex(quotes.quotes, num, setNum));
+    setQuote(quotes.quotes[num].quote);
+    setAuthor(quotes.quotes[num].author);
+  };
+
   return (
     <div className="quoteGenerator" style={{ color: bgColor }}>
       <Quote quote={quote} />
