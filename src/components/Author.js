d61ee87c43afe6c74878a626818dd0987e1f74be
Niels Metselaar <metsniels@gmail.com>
2021-07-01 15:07:06 +0200

    removed react imports from components

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function (props) {
   const { bgColor, author } = props;
   return (
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function (props) {
   const { randomQuote, bgColor } = props;
   return (
